129491adfb4dc14dfdd25f6275d9510159bc7cbd
Pham Thang <phamthang2062@gmail.com>
2026-06-29 22:18:23 +0700

    testing

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,8 @@
-function startGame(){
-    const ten=document.getElementById("playerName").value.trim();
-    if(ten===""){
+function startGame() {
+    const ten = document.getElementById("playerName").value.trim();
+    if (ten === "") {
         alert("Chưa nhập tên");
         return;
     }
-
-      window.location.href = "lv1(demo).html";
-
-
+window.location.replace('url')
 }
